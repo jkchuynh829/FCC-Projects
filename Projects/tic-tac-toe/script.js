b69e8cec66b6298812c01e4624fce7d34f1f53d4
Jimmy Huynh <jkchuynh829@gmail.com>
2016-09-17 01:30:58 -0700

    quick update

--- Projects/tic-tac-toe/script.js
+++ Projects/tic-tac-toe/script.js
@@ -1,11 +1,9 @@
 $(function() {
-
     var modeXO = "X";
     var lastMove = [];
 
     $(".gameBoard").hide();
     $(".selectScreen").hide();
-
     //CHOOSE NUMBER OF PLAYERS
     $("#one").click(function() {
         console.log(this.id + " player mode begin");
